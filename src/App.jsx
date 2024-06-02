57c03e4ee2c13b3394a0349e8c50d9881bc55801
draganjareb <dragan931@live.com>
2024-06-02 20:07:43 +0200

    create Contact page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navigation from "./features/Navigation/Navigation";
 import "./App.css";
 import Homepage from "./features/HomePage/Homepage";
 import About from "./features/About/About";
+import Contact from "./features/Contact/Contact";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
     </div>
